8e2df2a79b3da05a8f242b2c55f1e42aee8dc0a1
oussamaibra <oussamabr58@gmail.com>
2024-08-06 16:32:13 +0100

    notification

---
+++ src/main/java/com/example/Task_Management/services/NotificationService.java
@@ -0,0 +1,55 @@
+package com.example.Task_Management.services;
+
+import com.example.Task_Management.dto.CountType;
+import com.example.Task_Management.entities.Task;
+import com.example.Task_Management.repositories.TaskRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@AllArgsConstructor
+public class TaskService {
+
+    private TaskRepository taskRepository;
+
+    public List<Task> getTasks()
+    {
+        return taskRepository.getAllTaskByDueDate();
+    }
+    
+    public List<Task> getTasksByUserId(Long user_id)
+    {
+    		System.out.println("eeeeeeeeeeeeee"+user_id);
+        return taskRepository.getAllTaskByUserId(user_id);
+    }
+
+    public Optional<Task> getTaskById(Long id)
+    {
+        return taskRepository.findById(id);
+    }
+
+    public Task save(Task task)
+    {
+        return taskRepository.saveAndFlush(task);
+    }
+
+    public boolean existsById(Long id)
+    {
+        return taskRepository.existsById(id);
+    }
+
+    public void deleteTask(Long id)
+    {
+        taskRepository.deleteById(id);
+    }
+
+    public List<CountType> getPercentageGroupByType()
+    {
+        return taskRepository.getPercentageGroupByType();
+
+    }
+
+}
